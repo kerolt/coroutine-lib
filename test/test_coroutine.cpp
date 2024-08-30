3227b4e42574b85955e24510472162fc28921bfc
Kerolt <1517482303@qq.com>
2024-08-30 11:34:15 +0800

    修改test_coroutine.cpp，删除scheduler，采用更加直观的方式测试yield和resume

--- test/test_coroutine.cpp
+++ test/test_coroutine.cpp
@@ -1,57 +1,29 @@
 #include "coroutine.h"
 #include <iostream>
-#include <vector>
 
-class Scheduler {
-public:
-    // 添加协程调度任务
-    void schedule(std::shared_ptr<Coroutine> task) {
-        m_tasks.push_back(task);
-    }
+void Foo(int i) {
+    std::cout << "test_coroutine " << i + 1 << " start\n";
+    Coroutine::GetNowCoroutine()->Yield();
+    std::cout << "test_coroutine " << i + 1 << " end\n";
+}
 
-    // 执行调度任务
-    void run() {
-        std::cout << " number " << m_tasks.size() << std::endl;
+void TestCoroutine() {
+    Coroutine::GetNowCoroutine();
 
-        std::shared_ptr<Coroutine> task;
-        auto it = m_tasks.begin();
-        while (it != m_tasks.end()) {
-            // 迭代器本身也是指针
-            task = *it;
-            // 由主协程切换到子协程，子协程函数运行完毕后自动切换到主协程
-            task->Resume();
-            it++;
-        }
-        m_tasks.clear();
-    }
+    auto co1 = std::make_shared<Coroutine>(std::bind(Foo, 0), 0, false);
+    auto co2 = std::make_shared<Coroutine>(std::bind(Foo, 1), 0, false);
+    co1->Resume();
+    co2->Resume();
 
-private:
-    // 任务队列
-    std::vector<std::shared_ptr<Coroutine>> m_tasks;
-};
-
-void test_coroutine(int i) {
-    std::cout << "hello world " << i << std::endl;
+    co1->Resume();
+    co2->Resume();
 }
 
 int main() {
-    // 初始化当前线程的主协程
-    Coroutine::GetNowCoroutine();
-
-    // 创建调度器
-    Scheduler sc;
-
-    // 添加调度任务（任务和子协程绑定）
-    for (auto i = 0; i < 20; i++) {
-        // 创建子协程
-        // 使用共享指针自动管理资源 -> 过期后自动释放子协程创建的资源
-        // bind函数 -> 绑定函数和参数用来返回一个函数对象
-        std::shared_ptr<Coroutine> co = std::make_shared<Coroutine>(std::bind(test_coroutine, i), 0, false);
-        sc.schedule(co);
-    }
-
-    // 执行调度任务
-    sc.run();
-
-    return 0;
+    TestCoroutine();
+    // 输出应为：
+    // test_coroutine 1 start
+    // test_coroutine 2 start
+    // test_coroutine 1 end
+    // test_coroutine 2 end
 }
